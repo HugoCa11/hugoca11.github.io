555918fc87f5706a3bfcceb412bf5faf6f5b10ad
HugoCa11 <hugoi11@hotmail.com>
2022-08-02 23:10:45 -0700

    feat: photos and some descriptions added on about me section

--- assets/js/main.js
+++ assets/js/main.js
@@ -332,4 +332,57 @@
 
 			});
 
+			let slideIndex = 0;
+			showSlides();
+			//showSlides("mySlides2");
+
+			function showSlides() {
+			let i;
+			let slides = document.getElementsByClassName("mySlides");
+			
+			for (i = 0; i < slides.length; i++) {
+				slides[i].style.display = "none";  
+			}
+			slideIndex++;
+			if (slideIndex > slides.length) {slideIndex = 1}    
+			
+			slides[slideIndex-1].style.display = "block";  
+			setTimeout(showSlides, 2000); // Change image every 2 seconds
+			}
+
+			let slideIndex2 = 0;
+			showSlides2();
+
+			function showSlides2() {
+				let i;
+				let slides = document.getElementsByClassName("mySlides2");
+				
+				for (i = 0; i < slides.length; i++) {
+					slides[i].style.display = "none";  
+				}
+				slideIndex2++;
+				if (slideIndex2 > slides.length) {slideIndex2 = 1}    
+				
+				slides[slideIndex2-1].style.display = "block";  
+				setTimeout(showSlides2, 2000); // Change image every 2 seconds
+			}
+
+			let slideIndex3 = 0;
+			showSlides3();
+	
+			function showSlides3() {
+				let i;
+				let slides = document.getElementsByClassName("mySlides3");
+					
+				for (i = 0; i < slides.length; i++) {
+					slides[i].style.display = "none";  
+				}
+				slideIndex3++;
+				if (slideIndex3 > slides.length) {slideIndex3 = 1}    
+					
+				slides[slideIndex3-1].style.display = "block";  
+				setTimeout(showSlides3, 2000); // Change image every 2 seconds
+			}
+	
+
 })(jQuery);
